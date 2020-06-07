5526e5c9ade589f3183b332c313d7870e3be9ca0
Gabriel Alexandre <gabriel.15afs@gmail.com>
2020-06-07 12:07:00 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,70 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let mylet = "She"
+// var myvar = "niece"
+// const myconst = "is"
+
+// document.write(mylet + myconst + myvar)
+
+// string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso também é uma string'
+// const s3 = `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possui propriedade e funcionalidade
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+//Array - Vetores - coleção de dados
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de Código
+
+// registrar função
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop - repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
